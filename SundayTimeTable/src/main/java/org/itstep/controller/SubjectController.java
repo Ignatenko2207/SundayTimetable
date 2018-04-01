8f4489325bfe8329573a249398e36211530e13e8
dimoncheg38@gmail.com <shalaevdmitriy99@gmail.com>
2018-04-01 13:32:22 +0300

    Modify SubjectController

--- SundayTimeTable/src/main/java/org/itstep/controller/SubjectController.java
+++ SundayTimeTable/src/main/java/org/itstep/controller/SubjectController.java
@@ -1,8 +1,11 @@
 package org.itstep.controller;
 
-import javax.security.auth.Subject;
+
 
 import org.itstep.model.Lesson;
+import org.itstep.model.Subject;
+import org.itstep.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +19,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SubjectController {
 
+	@Autowired
+	SubjectService subjectService;
+	
 	@PostMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
 	ResponseEntity save(@RequestParam Subject subject) {
-
+		if(subjectService.save(subject) != null) {
+			return new ResponseEntity(HttpStatus.OK);
+		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 
 	@PutMapping(consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE })
 	ResponseEntity update(@RequestParam Subject subject) {
-
+		if(subjectService.update(subject) != null) {
+			return new ResponseEntity(HttpStatus.OK);
+		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 
 	@GetMapping(path = "/get-one", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
-	ResponseEntity<Subject> getOne(@RequestParam Integer id) {
-		// Subject subject = SubjectService.get(id);
+	ResponseEntity<Subject> getOne(@RequestParam String name) {
+		 Subject subject = subjectService.get(name);
+		if( subject != null) {
+			return new ResponseEntity(subject, HttpStatus.OK);
+		}
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 
 	@DeleteMapping
-	ResponseEntity delete(@RequestParam Integer id) {
+	ResponseEntity delete(@RequestParam String name ) {
+		subjectService.delete(name);
 		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 	}
 
