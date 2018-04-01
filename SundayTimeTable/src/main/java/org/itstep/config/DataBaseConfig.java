e915a4651fca037183be2a1f6f763a3faa28d3c4
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-01 13:33:56 +0300

    Add DB config

--- SundayTimeTable/src/main/java/org/itstep/config/DataBaseConfig.java
+++ SundayTimeTable/src/main/java/org/itstep/config/DataBaseConfig.java
@@ -1,6 +1,11 @@
 package org.itstep.config;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -16,42 +21,54 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 public class DataBaseConfig {
 
 	@Autowired
-    JpaVendorAdapter jpaVendorAdapter;
+	JpaVendorAdapter jpaVendorAdapter;
 
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setName("time_table")
-                .setType(EmbeddedDatabaseType.H2)
-                .build();
-    }
+	@Bean
+	public DataSource dataSource() {
 
-    @Bean
-    public EntityManager entityManager() {
-        return entityManagerFactory().createEntityManager();
-    }
+		HikariConfig config = new HikariConfig();
 
-    @Bean
-    public EntityManagerFactory entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-        lef.setDataSource(dataSource());
-        lef.setJpaVendorAdapter(jpaVendorAdapter);
-        lef.setPackagesToScan("org.itstep.*");
-        lef.afterPropertiesSet();
-        return lef.getObject();
-    }
+		HikariDataSource ds;
 
-    private Properties jpaProperties() {
+		config.setJdbcUrl("jdbc:h2:~/sunday_timetable");
+		config.setUsername("alex");
+		config.setPassword("12345");
+		config.setDriverClassName("");
+		config.addDataSourceProperty("cachePrepStmts", "true");
+		config.addDataSourceProperty("prepStmtCacheSize", "250");
+		ds = new HikariDataSource(config);
+
+		return ds.getDataSource();
+	}
+
+	@Bean
+	public EntityManager entityManager() {
+		return entityManagerFactory().createEntityManager();
+	}
+
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+		lef.setDataSource(dataSource());
+		lef.setJpaVendorAdapter(jpaVendorAdapter);
+		lef.setPackagesToScan("org.itstep.*");
+		return lef.getObject();
+	}
+
+	private Properties jpaProperties() {
 		Properties prop = new Properties();
 		return null;
 	}
 
 	@Bean
-    public PlatformTransactionManager transactionManager() {
-        return new JpaTransactionManager(entityManagerFactory());
-    }
+	public PlatformTransactionManager transactionManager() {
+		return new JpaTransactionManager(entityManagerFactory());
+	}
 }
